70df440808368e44707379717347716c85483386
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-05-10 20:47:23 +0300

    post-contact, put-contact, add validate Joi

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,8 @@
+const { json } = require("express");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 const path = require("path");
-//const { nanoid } = require('nanoid');
+const { stringify } = require("querystring");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
@@ -15,16 +17,34 @@ const getContactById = async (contactId) => {
   return contactById || null;
 };
 
+const addContact = async (body) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
 // const removeContact = async (contactId) => {}
 
-// const addContact = async (body) => {}
-
-// const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
   getContactById,
+  addContact,
   //removeContact,
-  //addContact,
-  //updateContact,
+  updateContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,23 @@
 const express = require("express");
 
+const Joi = require("joi");
+
 const contacts = require("../../models/contacts");
 const { HttpError } = require("../../helpers");
 
 const router = express.Router();
+
+const addSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -28,16 +42,38 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-// router.post('/', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, (message = "missing required name field"));
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // router.delete('/:contactId', async (req, res, next) => {
 //   res.json({ message: 'template message' })
 // })
 
-// router.put('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, (message = "missing fields"));
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
